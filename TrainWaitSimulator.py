1daaf6727d8025594085707c2f00aff11cedb772
Alan <a.nicholson2358@gmail.com>
2026-05-14 21:20:32 +0100

    project idea draft

---
+++ TrainWaitSimulator.py
@@ -0,0 +1,17 @@
+
+import time
+
+# Making a simulator that waits for a train. We will need: #
+
+"""
+
+
+wait_train() function
+closing_door() function
+while loop on time
+while loop on time door is taking to close
+
+"""
+
+print('Welcome to the train wait simulator!')
+print('Choose which train you want to wait for: ')
