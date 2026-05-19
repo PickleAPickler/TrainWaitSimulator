b9df08155202837516d2426b6ca471875be9052d
Alan <a.nicholson2358@gmail.com>
2026-05-19 20:39:13 +0100

    while True loop and output messages created

--- TrainWaitSimulator.py
+++ TrainWaitSimulator.py
@@ -1,10 +1,19 @@
 
-import time
+# import time
 
 # Making a simulator that waits for a train. We will need: #
 
 """
 
+Prerequisite tasks/projects:
+
+- Familiarity with    time   module
+- Sleep module?
+- Make a countdown timer 
+
+
+
+Potential functions, variables etc... 
 
 wait_train() function
 closing_door() function
@@ -17,7 +26,7 @@ Keep it simple. Just have a list of trains + their times
 You choose a train, you wait X seconds, you're given the option:
  Ready to board y/n?
  Y = you have borded the train!
- N = choose another train or press 'X' to leave the station
+ N = choose another train or press 'X' to leave the stationx
 
 Ideas:
 
@@ -30,8 +39,26 @@ print('These are the trains and when they arrive: \n' \
 'Nottingham | 1 min \n' \
 'London | 30 secs \n' \
 'Sandhurst | 10 secs')
-print(input('Choose which train you want to wait for: ')) 
-
 
 Trains = ['Nottingham', 'London', 'Sandhurst']
+
+
+while True:
+    user_input = (input('Choose which train you want to wait for: ')) 
+    if user_input == "q":
+        break
+
+    if user_input not in Trains:
+        continue
+
+    """Output messages:"""
+    
+    if user_input == 'Nottingham':
+        print('Have a nice time in Nottingham!')
+
+    if user_input == 'London':
+        print('Have a nice time in London!')
+
+    if user_input == 'Sandhurst':
+        print('Have a nice time in Sandhurst!')
 
